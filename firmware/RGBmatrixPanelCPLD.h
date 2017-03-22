2f36eec66f0c3afc5a6f2725cfb6c52be726d480
ReclaimerLabs <jason@reclaimerlabs.com>
2017-03-21 22:29:19 -0700

    Revert "added double buffering"
    This reverts commit 937fc958ddf98a44a37db8b3873e8dda0fe42579.

--- firmware/RGBmatrixPanelCPLD.h
+++ firmware/RGBmatrixPanelCPLD.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 Jason Cerundolo
+Copyright (c) 2016 Jason Cerundolo
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -45,42 +45,47 @@ transfer speed.
 #define _RGBMATRIXPANELCPLD_H
 
 #include "Adafruit_mfGFX/Adafruit_mfGFX.h"
+#define pgm_read_byte(addr) (*(const uint8_t *)(addr))
+#define PROGMEM
 
 class RGBmatrixPanelCPLD : public Adafruit_GFX {
     
     public: 
+    
+        /* 
+            Constructor for the matrix panel
+            
+            Assumptions: 32 x 32 panels, 4 color planes (ie 12 bit color per pixel)
+            
+        */
         RGBmatrixPanelCPLD(uint16_t x, uint16_t y);
-        RGBmatrixPanelCPLD(uint16_t x, uint16_t y, uint16_t d);
-        RGBmatrixPanelCPLD(uint16_t x, uint16_t y, uint16_t d, bool use_double_buffer);
         
-        void                begin(void);
-        void                drawPixel(int16_t x, int16_t y, uint16_t c);
-        void                fillScreen(uint16_t c);
-        void                resync(void);
-        void                updateDisplay(void);
-        void                swapBuffers(void);
+        void begin(void);
+        void drawPixel(int16_t x, int16_t y, uint16_t c);
+        void fillScreen(uint16_t c);
+        void updateDisplay(void);
+        void resync(void);
         
-        uint16_t            Color333(uint8_t r, uint8_t g, uint8_t b);
-        uint16_t            Color444(uint8_t r, uint8_t g, uint8_t b);
-        uint16_t            Color888(uint8_t r, uint8_t g, uint8_t b);
-        uint16_t            Color888(uint8_t r, uint8_t g, uint8_t b, boolean gflag);
-        uint16_t            ColorHSV(long hue, uint8_t sat, uint8_t val, boolean gflag);
+        uint16_t Color333(uint8_t r, uint8_t g, uint8_t b);
+        uint16_t Color444(uint8_t r, uint8_t g, uint8_t b);
+        uint16_t Color888(uint8_t r, uint8_t g, uint8_t b);
+        uint16_t Color888(uint8_t r, uint8_t g, uint8_t b, boolean gflag);
+        uint16_t ColorHSV(long hue, uint8_t sat, uint8_t val, boolean gflag);
         
-        volatile uint8_t    rowComplete;
-        int8_t              initStatus;
-
+        volatile uint8_t rowComplete;
+        int8_t initStatus;
+    
     private:
-        int8_t              init();
-        volatile uint16_t   row, plane;
-        uint16_t            height, width, depth, row_size, plane_size;
-        uint16_t            transfer_row_width;
-        uint8_t* volatile   front_buffer;
-        uint8_t* volatile   back_buffer;
-        int                 clr_pin, oe_pin;
-        bool                double_buffer_enabled;
-        volatile bool       resync_flag;
-        volatile bool       swap_requested;
-        
+        int8_t init(uint16_t x, uint16_t y, uint16_t p);
+        volatile uint16_t row, plane;
+        uint16_t height, width, depth, row_size, plane_size;
+        uint16_t transfer_row_width;
+        volatile uint8_t *buffPtr;
+        uint8_t *matrixbuff[2];
+        uint8_t *zerobuff;
+        int clr_pin, oe_pin;
+        volatile bool resync_flag;
+        volatile uint8_t delay_done;
 };
 
 #endif //_RGBMATRIXPANELCPLD_H
